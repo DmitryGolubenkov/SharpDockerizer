c64dc20285a99e56ed4aff55bfad7a948beefd52
DmitryGolubenkov <xxdimonxx15@yandex.ru>
2024-03-16 19:20:40 +0300

    Updated ASP NET image version selector

--- src/SharpDockerizer.AppLayer/Services/Project/AspNetDockerImageVersionSelector.cs
+++ src/SharpDockerizer.AppLayer/Services/Project/AspNetDockerImageVersionSelector.cs
@@ -1,19 +1,29 @@
 ﻿using SharpDockerizer.AppLayer.Contracts;
 
 namespace SharpDockerizer.AppLayer.Services.Project;
+
 public class AspNetDockerImageVersionSelector : IAspNetDockerImageVersionSelector
 {
-    // TODO: temporary implementation for generic purposes. Maybe provide some way to choose exact
-    // version in GUI? Make GET requests to docker hub API to find avaliable images?  
-    public string GetLinkToImageForVersion(string version) => version is not null ? version.Trim() switch
-    {
-        "netcoreapp3.1" => "mcr.microsoft.com/dotnet/aspnet:3.1",
-        "net5.0" => "mcr.microsoft.com/dotnet/aspnet:5.0",
-        "net6.0" => "mcr.microsoft.com/dotnet/aspnet:6.0",
-        "net7.0" => "mcr.microsoft.com/dotnet/aspnet:7.0",
-        "net8.0" => "mcr.microsoft.com/dotnet/aspnet:8.0",
-        "net9.0" => "mcr.microsoft.com/dotnet/aspnet:9.0",
-        _ => "mcr.microsoft.com/dotnet/aspnet"
-    } : "mcr.microsoft.com/dotnet/aspnet";
+    private const string BaseUrl = "mcr.microsoft.com/dotnet/aspnet:";
 
+    public string GetLinkToImageForVersion(string version)
+    {
+        if (string.IsNullOrWhiteSpace(version))
+        {
+            return BaseUrl;
+        }
+
+        version = version.Trim().ToLowerInvariant();
+
+        if ((version.StartsWith("net") 
+            && version.Length > 3 
+            && version[3..].All(char.IsDigit)) 
+            || version.StartsWith("netcoreapp"))
+        {
+            string versionNumber = version.StartsWith("netcoreapp") ? version["netcoreapp".Length..] : version[3..];
+            return $"{BaseUrl}{versionNumber}";
+        }
+
+        return BaseUrl;
+    }
 }
